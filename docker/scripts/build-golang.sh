c0caeb580af7eb1430d1926ae8efe65b35204795
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2020-11-08 17:20:10 +0100

    Updated to the latest versions

--- docker/scripts/build-golang.sh
+++ docker/scripts/build-golang.sh
@@ -3,7 +3,7 @@
 set -ex
 mkdir -p ${CROSS_ROOT}/bootstrap
 if [ ! -f "golang-bootstrap.tar.gz" ]; then
-  wget -q "$GOLANG_BOOTSTRAP_URL" -O golang-bootstrap.tar.gz
+  wget -q "https://dl.google.com/go/go${GOLANG_BOOTSTRAP_VERSION}.tar.gz" -O golang-bootstrap.tar.gz
 fi
 echo "$GOLANG_BOOTSTRAP_SHA256  golang-bootstrap.tar.gz" | sha256sum -c -
 tar -C ${CROSS_ROOT}/bootstrap -xzf golang-bootstrap.tar.gz
@@ -14,7 +14,7 @@ export GOROOT_BOOTSTRAP=${CROSS_ROOT}/bootstrap/go
 
 cd /build
 if [ ! -f "golang.tar.gz" ]; then
-  wget -q "$GOLANG_SRC_URL" -O golang.tar.gz
+  wget -q "https://golang.org/dl/go${GOLANG_VERSION}.src.tar.gz" -O golang.tar.gz
 fi
 echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
 tar -C ${CROSS_ROOT} -xzf golang.tar.gz
@@ -22,5 +22,11 @@ rm golang.tar.gz
 cd ${CROSS_ROOT}/go/src
 ./make.bash
 
-CC_FOR_TARGET=${GOLANG_CC} CXX_FOR_TARGET=${GOLANG_CXX} GOOS=${GOLANG_OS} GOARCH=${GOLANG_ARCH} GOARM=${GOLANG_ARM} CGO_ENABLED=1 ./make.bash --no-clean
+CC_FOR_TARGET=${GOLANG_CC} \
+  CXX_FOR_TARGET=${GOLANG_CXX} \
+  GOOS=${GOLANG_OS} \
+  GOARCH=${GOLANG_ARCH} \
+  GOARM=${GOLANG_ARM} \
+  CGO_ENABLED=1 \
+  ./make.bash --no-clean
 rm -rf ${CROSS_ROOT}/bootstrap ${CROSS_ROOT}/go/pkg/bootstrap

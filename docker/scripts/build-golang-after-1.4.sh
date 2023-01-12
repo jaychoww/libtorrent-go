8955fe6314301c8d051b380464d5b6aad203a234
Jay Chow <jayzml@gmail.com>
2023-01-12 15:55:33 +0800

    Merge branch 'darwin_arm64'

--- docker/scripts/build-golang-after-1.4.sh
+++ docker/scripts/build-golang-after-1.4.sh
@@ -5,14 +5,21 @@ scripts_path=$(dirname "$(readlink -f "$0")")
 source "${scripts_path}/common.sh"
 
 mkdir -p ${CROSS_ROOT}/bootstrap
-# install go
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-arm64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-# build bootstrap
+if [ ! -f "golang-bootstrap.tar.gz" ]; then
+  cd ${CROSS_ROOT}/bootstrap
+  wget -q "https://go.dev/src/bootstrap.bash?m=text" -O bootstrap.bash
+  chmod 755 bootstrap.bash
+  GOOS=${GOLANG_OS} GOARCH=${GOLANG_ARCH} ./bootstrap.bash
 
+fi
+# echo "$GOLANG_BOOTSTRAP_SHA256  golang-bootstrap.tar.gz" | sha256sum -c -
+#tar -C ${CROSS_ROOT}/bootstrap -xzf golang-bootstrap.tar.gz
+#rm golang-bootstrap.tar.gz
+ls -ltr
 
-#cd ${CROSS_ROOT}/bootstrap/go/src
-# run ./make.bash
+cd ${CROSS_ROOT}/bootstrap/go/src
+run ./make.bash
+export GOROOT_BOOTSTRAP=${CROSS_ROOT}/bootstrap/go
 
 cd /build
 if [ ! -f "golang.tar.gz" ]; then
@@ -21,16 +28,9 @@ fi
 echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
 tar -C ${CROSS_ROOT} -xzf golang.tar.gz
 rm golang.tar.gz
-mkdir ${CROSS_ROOT}/go/script
-cd ${CROSS_ROOT}/go/script
-cp /build/bootstrap.bash ./
+cd ${CROSS_ROOT}/go/src
+run ./make.bash
 
-GOOS=${GOLANG_OS} GOARCH=${GOLANG_ARCH} ./bootstrap.bash
-export GOROOT_BOOTSTRAP=${CROSS_ROOT}/go-darwin-arm64-bootstrap
-
-# run ./make.bash
-
- cd ${CROSS_ROOT}/go/src
 CC_FOR_TARGET=${GOLANG_CC} \
   CXX_FOR_TARGET=${GOLANG_CXX} \
   GOOS=${GOLANG_OS} \

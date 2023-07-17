9610c539d7e0ae09e1aac4a4e44ff0a3cdc17897
Denis Kuzmenok <denis.kuzmenok@gmail.com>
2023-07-17 15:59:24 +0200

    Updated Go to 1.20.6, Go bootstrap to 1.19.11

--- docker/scripts/build-golang.sh
+++ docker/scripts/build-golang.sh
@@ -6,20 +6,18 @@ source "${scripts_path}/common.sh"
 
 mkdir -p ${CROSS_ROOT}/bootstrap
 if [ ! -f "golang-bootstrap.tar.gz" ]; then
-  wget -q "https://dl.google.com/go/go${GOLANG_BOOTSTRAP_VERSION}.tar.gz" -O golang-bootstrap.tar.gz
+  wget -q "https://golang.org/dl/go${GOLANG_BOOTSTRAP_VERSION}.linux-amd64.tar.gz" -O golang-bootstrap.tar.gz
 fi
 echo "$GOLANG_BOOTSTRAP_SHA256  golang-bootstrap.tar.gz" | sha256sum -c -
 tar -C ${CROSS_ROOT}/bootstrap -xzf golang-bootstrap.tar.gz
 rm golang-bootstrap.tar.gz
-cd ${CROSS_ROOT}/bootstrap/go/src
-run ./make.bash
 export GOROOT_BOOTSTRAP=${CROSS_ROOT}/bootstrap/go
 
 cd /build
 if [ ! -f "golang.tar.gz" ]; then
   wget -q "https://golang.org/dl/go${GOLANG_VERSION}.src.tar.gz" -O golang.tar.gz
 fi
-echo "$GOLANG_SRC_SHA256  golang.tar.gz" | sha256sum -c -
+echo "$GOLANG_SHA256  golang.tar.gz" | sha256sum -c -
 tar -C ${CROSS_ROOT} -xzf golang.tar.gz
 rm golang.tar.gz
 cd ${CROSS_ROOT}/go/src
@@ -31,5 +29,6 @@ CC_FOR_TARGET=${GOLANG_CC} \
   GOARCH=${GOLANG_ARCH} \
   GOARM=${GOLANG_ARM} \
   CGO_ENABLED=1 \
-  ./make.bash --no-clean
+  ./make.bash
+${CROSS_ROOT}/go/bin/go install std cmd
 rm -rf ${CROSS_ROOT}/bootstrap ${CROSS_ROOT}/go/pkg/bootstrap
